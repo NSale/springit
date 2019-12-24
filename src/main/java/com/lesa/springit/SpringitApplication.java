b9e2c587eecfc4f464cd9a2fac543f78b67c66c3
lesa <lesa@lesa-MS-7996>
2019-12-24 22:47:31 +0100

    Initial commit

---
+++ src/main/java/com/lesa/springit/SpringitApplication.java
@@ -0,0 +1,14 @@
+package com.lesa.springit;
+
+import static org.springframework.boot.SpringApplication.run;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringitApplication {
+
+	public static void main(String[] args) {
+		run(SpringitApplication.class, args);
+	}
+
+}
