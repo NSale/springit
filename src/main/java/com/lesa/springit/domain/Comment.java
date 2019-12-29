0cbca66d82035e056ec5f0df8a7e742d5eaa95a1
NSale <gospasrb@yahoo.com>
2019-12-29 22:30:31 +0100

    add mappings from link to comment and vice versa

--- src/main/java/com/lesa/springit/domain/Comment.java
+++ src/main/java/com/lesa/springit/domain/Comment.java
@@ -3,6 +3,7 @@ package com.lesa.springit.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,5 +17,9 @@ public class Comment {
 	@GeneratedValue
 	private Long id;
 	private String body;
+	
+	// link
+	@ManyToOne
+	private Link link;
 	
 }
