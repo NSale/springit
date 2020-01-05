bf6c4b2e23a6e2ddf9b872579f69958ccbaa4bd2
NSale <gospasrb@yahoo.com>
2020-01-05 23:18:51 +0100

    add data and schema sql files, some app properties and refactor some code

--- src/main/java/com/lesa/springit/domain/Comment.java
+++ src/main/java/com/lesa/springit/domain/Comment.java
@@ -7,6 +7,7 @@ import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Entity
 @NoArgsConstructor
@@ -16,10 +17,17 @@ public class Comment extends Auditable {
 	@Id
 	@GeneratedValue
 	private Long id;
+	@NonNull
 	private String body;
 	
 	// link
 	@ManyToOne
+	@NonNull
 	private Link link;
+
+	public Comment(@NonNull String body, @NonNull Link link) {
+		this.body = body;
+		this.link = link;
+	}
 	
 }
