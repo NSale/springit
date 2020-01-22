c0112aef4285f6656b8eeb7c7e7948db64eef6b0
NSale <gospasrb@yahoo.com>
2020-01-22 22:37:45 +0100

    update link controller and html file

--- src/main/java/controller/LinkController.java
+++ src/main/java/controller/LinkController.java
@@ -1,9 +1,13 @@
 package controller;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.lesa.springit.domain.Link;
 import com.lesa.springit.repository.LinkRepository;
 
 @Controller
@@ -19,6 +23,18 @@ public class LinkController {
 	public String list(Model model) {
 		model.addAttribute("links", linkRepository.findAll());
 		return "link/list";
+	}
+	
+	@GetMapping("/link/{id}")
+	public String read(@PathVariable Long id, Model model) {
+		Optional<Link> link = linkRepository.findById(id);
+		
+		if(link.isPresent()) {
+			model.addAttribute("link", link.get());
+			return "link/view";
+		} else {
+			return "redirect:/";
+		}
 	}
 	
 }
