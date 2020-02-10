f591b65b09683d61d5acdcf2241e068071250e11
NSale <gospasrb@yahoo.com>
2020-02-10 21:55:24 +0100

    changed properties and security configurations

--- src/main/java/com/lesa/springit/SpringitApplication.java
+++ src/main/java/com/lesa/springit/SpringitApplication.java
@@ -5,11 +5,13 @@ import static org.springframework.boot.SpringApplication.run;
 import org.ocpsoft.prettytime.PrettyTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.lesa.springit.controller.LinkController;
+import com.lesa.springit.repository.UserRepository;
 import com.lesa.springit.service.BeanUtil;
 
 import bootstrap.DatabaseLoader;
@@ -25,7 +27,7 @@ import security.SecurityConfiguration;
 public class SpringitApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
-	
+		
 	public static void main(String[] args) {
 		run(SpringitApplication.class, args);
 	}
@@ -34,5 +36,5 @@ public class SpringitApplication {
 	PrettyTime prettyTime() {
 		return new PrettyTime();
 	}
-	
+		
 }
